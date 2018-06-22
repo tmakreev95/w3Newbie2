7d6424b623510ac6b88c81bcd92e7eb9d9d17744
Тодор Макрееев <35991272+tmakreev95@users.noreply.github.com>
2018-06-22 19:55:59 +0300

    Initial Commit

---
+++ animations/custom-animations.js
@@ -0,0 +1,9 @@
+/* Chanign Font Awesome on Click */
+$(function() {
+    $(".fun").click(function() {
+        $("#changer").toggleClass("far fa-caret-square-down far fa-caret-square-up");
+    });
+});
+/* Chanign Font Awesome on Click "<span class="fun" data-toggle="collapse" data-target="#emoji">
+<i class="far fa-caret-square-down" id="changer"></i></span>" */
+
---
+++ animations/scrollreveal_action.js
@@ -0,0 +1,6 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.container-fluid', { duration: 1000 });
+sr.reveal('.carousel', { duration: 1000 });
+sr.reveal('.jumbotron', { duration: 1000 });
+sr.reveal('.fun', { duration: 1000 });
